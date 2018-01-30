d50da075d150ce9c22c5a3250bd97f6809fa4566
Spanish Pear <106299@avondaleschool.nsw.edu.au>
2018-01-30 19:38:07 +1100

    Document.onload

--- assets/js/picturedropper.js
+++ assets/js/picturedropper.js
@@ -1,34 +1,36 @@
-var possibleImages = [
-  "assets/images/Moana.jpg",
-  "assets/images/GuitarPlaying.png",
-  "assets/images/Cheese.jpg",
-  "assets/images/CryptoPhoto.jpg",
-  "assets/images/LogoSmall.png",
-  "assets/images/Group5.jpg"
+document.onload = function(){
+  var possibleImages = [
+    "assets/images/Moana.jpg",
+    "assets/images/GuitarPlaying.png",
+    "assets/images/Cheese.jpg",
+    "assets/images/CryptoPhoto.jpg",
+    "assets/images/LogoSmall.png",
+    "assets/images/Group5.jpg"
   ];
 
-document.body.addEventListener("mousemove", function(ev) {
-    if (Math.random() < 0.85) {
-        return;
-    }
+  document.body.addEventListener("mousemove", function(ev) {
+      if (Math.random() < 0.85) {
+          return;
+      }
 
-    const index = Math.round(Math.random() * possibleImages.length);
-    const image = possibleImages[index];
+      const index = Math.round(Math.random() * possibleImages.length);
+      const image = possibleImages[index];
 
-    const el = document.createElement("img");
-    document.body.appendChild(el);
+      const el = document.createElement("img");
+      document.body.appendChild(el);
 
-    el.classList.add('emoji');
-    el.src = image
-    el.offsetLeft;  // forces layout
+      el.classList.add('emoji');
+      el.src = image
+      el.offsetLeft;  // forces layout
 
-    el.style.left = (event.clientX-410) + 'px';
-    el.style.top = (event.clientY-50) + 'px';
-    el.style.transform = 'translate(' + (Math.random() * -1000 + 500) + 'px, 1200px) scale(0)';
+      el.style.left = (event.clientX-410) + 'px';
+      el.style.top = (event.clientY-50) + 'px';
+      el.style.transform = 'translate(' + (Math.random() * -1000 + 500) + 'px, 1200px) scale(0)';
 
 
-    el.addEventListener('transitionend', () => {
-        el.remove()
-    })
+      el.addEventListener('transitionend', () => {
+          el.remove()
+      })
 
-}, true);
+  }, true);
+}
